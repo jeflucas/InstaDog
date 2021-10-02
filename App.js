c622e246eb6eddc9a02409c6b0ab1b05a726cba9
jeflucas <jef.lucas@hotmail.co.uk>
2021-10-02 14:34:22 -0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  const [like, setLike] = useState(0);
-
-  var increment = () => {
-    setLike(like + 1);
-  };
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Like</Text>
-      <Text style={styles.text}>{like}</Text>
-      <TouchableOpacity>
-        <Text onPress={increment}>Mais 1</Text>
-      </TouchableOpacity>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 22,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
